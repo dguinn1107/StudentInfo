7e97f9a839d910d0d1356d98c17b934b0a42368c
Daigeona Guinn <dguinn1107@gmail.com>
2024-10-31 10:28:38 -0500

    this is a test to show how changes work in git

--- Form1.cs
+++ Form1.cs
@@ -21,6 +21,7 @@ namespace StudentInfo
         public Form1()
         {
             InitializeComponent();
+            //hi
         }
 
         private void Form1_Load(object sender, EventArgs e)
